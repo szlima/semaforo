4eb79b216a62e8c798ec58d9ed0fb05125f820a4
szlima <suzanelima@outlook.com>
2024-07-10 18:53:32 -0300

    Início do projeto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  
+  return (
+    <main className="main">
+      <h1 className="main__title">Semáforo</h1>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App.jsx';
+
+import './styles/variables.css';
+import './styles/reset.css';
+import './styles/index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+);
