44ca34de57d68733ff524fa9dac1b4567595ac9f
szlima <suzanelima@outlook.com>
2024-07-11 11:51:42 -0300

    Definição da mudança automática da luz acesa

--- src/components/TrafficLight.jsx
+++ src/components/TrafficLight.jsx
@@ -1,10 +1,21 @@
-
+import { useEffect } from 'react';
 
 import Light from './Light';
 
 import './styles/trafficLight.css';
 
 function TrafficLight({lights, selectedColor, changeSelectedColor}){
+
+    const changeColorAuto= () => {
+        changeSelectedColor(id =>
+            (id === (lights.length-1) ? 0 : (id+1))
+        );
+    };
+
+    useEffect(() => {
+        const timeout= setTimeout(changeColorAuto, 5000);
+        return () => clearTimeout(timeout);
+    }, [selectedColor]);
 
     return (
         <div className="traffic-light">
